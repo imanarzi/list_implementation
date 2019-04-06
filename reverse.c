a44aa7e3ef08cbee29b48aaf732ab89509b0b717
Iman Anwarzai <ianwarzai18@src-code.simons-rock.edu>
2019-04-05 22:03:41 -0400

    done

--- reverse.c
+++ reverse.c
@@ -11,24 +11,50 @@ void reverse_array(int * arr, int size){
 		arr[size - i - 1] = temp;
 	}
 }
-/*void reverse_list(){
-	
+Node * reverse_list(Node * head){
+	Node * curr = head;
+	Node * prev = NULL;
+	Node * next = NULL;
+	while(curr != NULL){
+		next = curr->next;
+		curr->next = prev;
+		prev = curr;
+		curr = next;
+	}
+	return prev;
+		
 
-}*/
+}
 int main(){
+	//initialize array
 	int * arr = malloc(sizeof(int) * 10);
         for(int i = 0; i < 10; i++)
 		arr[i] = i;
+	//reverse array and print results
 	reverse_array(arr, 10);
 	for(int i = 0; i < 10; i++)
-		printf("%d ", arr[i]);	
-	Node a; Node b; Node c;
+		printf("%d ", arr[i]);
+	printf("\n");	
+	free(arr);
+	
+	//initialize 3 nodes of a linked list
+	Node* a = malloc(sizeof(Node)); Node* b = malloc(sizeof(Node)); Node* c = malloc(sizeof(Node));
 	a->value = 1; 
 	a->next = b;
-	b.value = 2;
-	b.next = c;
-	c.value = 3;
-	c.next = NULL;
+	b->value = 2;
+	b->next = c;
+	c->value = 3;
+	c->next = NULL;
 	
+	//reverse linked list and print
+	a = reverse_list(a);	
+	Node* p  = a;
+	while(p){
+		printf("%d ", p->value);
+		Node * x = p;
+		free(x);
+		p = p->next;
+	}	
+	printf("\n");
 	return 0;	
 }
